83b4c1732b113010b595509f8dee182e140ccd31
Sharon <sharonalexander2596@gmail.com>
2018-11-03 16:43:19 +0530

    updated code updation_number:2

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -13,25 +13,32 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
 from bs4 import BeautifulSoup
-import urllib.request
 import re
+
+NUM_OF_MOBILES = 0
 
 class Amazon_all_mobile_scraper:
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = None
         self.delay = 20
         self.url = 'https://www.amazon.in/s/s/ref=sr_nr_p_89_1?bq=%28not+browse%3A%2714991705031%7C14991710031%7C14991712031%7C14991716031%7C14991714031%7C14991718031%27%29&fst=as%3Aoff&rh=n%3A976419031%2Cn%3A%21976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Cp_98%3A10440597031%2Cp_36%3A1500000-99999999%2Cp_89%3ASamsung&bbn=1805560031&ie=UTF8&qid=1541004646&rnid=3837712031'
         
     def load_amazon(self):
-        self.driver.get(self.url)
-        try:
-            wait = WebDriverWait(self.driver,self.delay)
-            wait.until(EC.presence_of_element_located((By.ID,"a-page")))
-            print("Page is ready.")
-        except TimeoutException:
-            print("Took too much time to load!")
-        except:
-            print("Something went wrong in loading part!!")
+        reload = 1
+        while reload == 1:
+            try:
+                self.driver = webdriver.Firefox()
+                self.driver.get(self.url)
+                wait = WebDriverWait(self.driver,self.delay)
+                wait.until(EC.presence_of_element_located((By.ID,"a-page")))
+                print("Page is ready.")
+                reload = 0
+            except TimeoutException:
+                print("Took too much time to load!")
+                self.driver.close()
+            except:
+                print("Something went wrong in loading part!!")
+                self.driver.close()
             
     def extract_list_of_mobiles(self):
         mobile_list_text = []
@@ -47,23 +54,38 @@ class Amazon_all_mobile_scraper:
     
     def extract_url_of_mobiles(self,mobiles):
         url_list = []
-        html_page = urllib.request.urlopen(self.url)
+        html_page = self.driver.page_source
         soup = BeautifulSoup(html_page,'lxml')
-        for link in soup.find_all("a",{"class":"a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"}):
-            url_list.append(link["href"])        
-        return url_list,soup
-    
+        NUM_OF_MOBILES = len(mobiles)
+        for i in range(NUM_OF_MOBILES):
+            var = soup.find("a",{"title":mobiles[i]})
+            url_list.append(var["href"])
+        return url_list
+        
+        
+        
     def exitAmazon(self):
         self.driver.close()
 
 scraper = Amazon_all_mobile_scraper()
 scraper.load_amazon()
 data = scraper.extract_list_of_mobiles()
-data_link,soup = scraper.extract_url_of_mobiles(data)
+data_link = scraper.extract_url_of_mobiles(data)
 scraper.exitAmazon()
 
-#print("Preview of scraped data.")
-#for i in range(3):
-#    print("Mobile:" + data[i])
-#    print("Link:" + data_link[i])
+# Removing commas in mobile names
+NUM_OF_MOBILES = len(data)
+mobile_names = []
+for i in range(NUM_OF_MOBILES):
+    mobile_names.append(re.sub(",","",data[i]))
+
+# Converting to csv file    
+with open('mobilename_link.csv','a') as f:
+    f.write('Mobile Name,Link\n')
+    for i in range(NUM_OF_MOBILES):
+        f.write(mobile_names[i] + "," + data_link[i] + "\n")
         
+        
+
+
+
