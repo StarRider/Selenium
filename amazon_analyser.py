2be8140cde78979beea127e58b18fd396bd6efed
Sharon <sharonalexander2596@gmail.com>
2018-11-06 17:55:17 +0530

    updated code updation_number:5

--- amazon_analyser.py
+++ amazon_analyser.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 
 filename = "mobilename_link.csv"
 amazon_data = pd.read_csv(filename)
+numerical_data = amazon_data.loc[:,['Customers','Deal_price','Retail_price','Mobile_rating']]
+
 
 price_part = amazon_data.loc[:,['Deal_price','Retail_price']]
 
@@ -29,8 +31,8 @@ n_cluster = np.arange(1,10) # chosen after looking at the plot w.r.t deal and re
 print('KMeans Model...')
 for clusters in n_cluster:
     kmeans = KMeans(n_clusters = clusters)
-    kmeans.fit(price_part)
-    score_of_model.append(kmeans.score(price_part))
+    kmeans.fit(numerical_data)
+    score_of_model.append(kmeans.score(numerical_data))
     print('.',end = "")
 
 # ploting the elbow curve
@@ -39,6 +41,127 @@ plt.xlabel('Clusters')
 plt.ylabel('Score')
 plt.title('The elbow curve')
 plt.show()
+
+# As per the elbow curve number of clusters = 3
+num_of_cluster = 3
+kmeans = KMeans(n_clusters = num_of_cluster)
+kmeans.fit(numerical_data)
+
+# Forming dataframe of cluster 
+cluster_group = pd.DataFrame(data = kmeans.labels_, columns = ['Group'])
+amazon_data = amazon_data.assign(Group = cluster_group)
+
+# Saving the file
+ml_data = amazon_data.iloc[:,1:]
+ml_data.to_csv('mobilename_link.csv')
+
+# deal price vs retail price
+for i in range(len(ml_data)):
+    grp = ml_data.iloc[i,5]
+    if grp == 0:
+        pattern = 'x'
+        color = 'r'
+    elif grp == 1:
+        pattern = 'o'
+        color = 'b'
+    elif grp == 2:
+        pattern = '*'
+        color = 'g'
+    plt.plot(ml_data.iloc[i,1],ml_data.iloc[i,4],color + pattern)
+    
+plt.xlabel('Deal price')
+plt.ylabel('Retail price')
+plt.title('Retail price vs Deal price with clusters')
+plt.show()
+
+# deal price vs Number of Customers
+for i in range(len(ml_data)):
+    grp = ml_data.iloc[i,5]
+    if grp == 0:
+        pattern = 'x'
+        color = 'r'
+    elif grp == 1:
+        pattern = 'o'
+        color = 'b'
+    elif grp == 2:
+        pattern = '*'
+        color = 'g'
+    plt.plot(ml_data.iloc[i,1],ml_data.iloc[i,0],color + pattern)
+    
+plt.xlabel('Deal price')
+plt.ylabel('Number of Customers')
+plt.title('Number of Customers vs Deal price with clusters')
+plt.show()
+
+# deal price vs ratings
+for i in range(len(ml_data)):
+    grp = ml_data.iloc[i,5]
+    if grp == 0:
+        pattern = 'x'
+        color = 'r'
+    elif grp == 1:
+        pattern = 'o'
+        color = 'b'
+    elif grp == 2:
+        pattern = '*'
+        color = 'g'
+    plt.plot(ml_data.iloc[i,1],ml_data.iloc[i,3],color + pattern)
+    
+plt.xlabel('Deal price')
+plt.ylabel('Ratings of Customers')
+plt.title('Ratings of Customers vs Deal price with clusters')
+plt.show()
+
+# Best choice among the clusters on the basis of number of rated customers and ratings
+max0 = 0
+max1 = 0
+max2 = 0
+maxname0 = ''
+maxname1 = ''
+maxname2 = ''
+for i in range(len(ml_data)):
+    grp = ml_data.iloc[i,5]
+    rating = ml_data.iloc[i,3]
+    num_of_customers = ml_data.iloc[i,0]
+    compound_rating = rating*num_of_customers
+    if grp == 0:
+        if max0 < compound_rating:
+            max0 = compound_rating
+            maxname0 = i
+    elif grp == 1:
+        if max1 < compound_rating:
+            max1 = compound_rating
+            maxname1 = i
+    elif grp == 2:
+        if max2 < compound_rating:
+            max2 = compound_rating
+            maxname2 = i
+
+print('Cluster 0s best phone:' + ml_data.iloc[maxname0,2],' Deal Price:',ml_data.iloc[maxname0,1] )
+print('Cluster 1s best phone:' + ml_data.iloc[maxname1,2],' Deal Price:',ml_data.iloc[maxname1,1] )
+print('Cluster 2s best phone:' + ml_data.iloc[maxname2,2],' Deal Price:',ml_data.iloc[maxname2,1] ) 
+
+best_data = [
+        ml_data.iloc[maxname0,[2,1]],
+        ml_data.iloc[maxname1,[2,1]],
+        ml_data.iloc[maxname2,[2,1]],
+        ]
+resultset = pd.DataFrame(data = best_data,
+                                    index = ['Cluster 0 best','Cluster 1 best','Cluster 2 best']
+                                    )
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- amazon_scraper.py
+++ amazon_scraper.py
@@ -16,6 +16,9 @@ from bs4 import BeautifulSoup
 import re
 from urllib.request import urlretrieve
 
+import pandas as pd
+import traceback
+
 NUM_OF_MOBILES = 0
 
 class Amazon_all_mobile_scraper:
@@ -23,13 +26,20 @@ class Amazon_all_mobile_scraper:
         self.driver = None
         self.delay = 20
         self.url = 'https://www.amazon.in/s/s/ref=sr_nr_p_89_1?bq=%28not+browse%3A%2714991705031%7C14991710031%7C14991712031%7C14991716031%7C14991714031%7C14991718031%27%29&fst=as%3Aoff&rh=n%3A976419031%2Cn%3A%21976420031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Cp_98%3A10440597031%2Cp_36%3A1500000-99999999%2Cp_89%3ASamsung&bbn=1805560031&ie=UTF8&qid=1541004646&rnid=3837712031'
+        self.page_link = [self.url]
         self.soup = None
+        self.driver = webdriver.Firefox()
+        
+    def setUrl(self,url):
+        self.url = url
+    
+    def get_page_link(self):
+        return self.page_link
         
     def load_amazon(self):
         reload = 1
         while reload == 1:
             try:
-                self.driver = webdriver.Firefox()
                 self.driver.get(self.url)
                 wait = WebDriverWait(self.driver,self.delay)
                 wait.until(EC.presence_of_element_located((By.ID,"a-page")))
@@ -41,9 +51,11 @@ class Amazon_all_mobile_scraper:
             except TimeoutException:
                 print("Took too much time to load!")
                 self.driver.close()
+                self.driver = webdriver.Firefox()
             except:
                 print("Something went wrong in loading part!!")
                 self.driver.close()
+                self.driver = webdriver.Firefox()
             
     def extract_list_of_mobiles(self):
         mobile_list_text = []
@@ -74,61 +86,191 @@ class Amazon_all_mobile_scraper:
             images.append(urlretrieve(tag["src"],filename))
             img_num = img_num + 1
         return images
+    
+    def extract_next_page_url(self):
+        # find all span with page links
+        var = self.soup.find_all("span",{"class":"pagnLink"})
+        # get the a tags
+        atag = []
+        for span in var:
+            atag.append(span.find("a"))
+        # get the href part
+        for tag in atag:
+            self.page_link.append('https://www.amazon.in' + tag["href"])
         
-    def extract_price_of_mobile(self):
-        # This function returns the deal and retail price of phones
+        
+    def extract_price_of_mobile2(self):
         deal_price = []
         retail_price = []
-        deal_price_tag = self.soup.find_all("span",{"class":"a-size-base a-color-price s-price a-text-bold"})
-        retail_price_tag = self.soup.find_all("span",{"class":"a-size-small a-color-secondary a-text-strike"})
-        #print(len(retail_price_tag))
-        #print(len(deal_price_tag))
-        for tag in range(len(deal_price_tag)):
-            string_deal_price = deal_price_tag[tag].text
-            string_retail_price = retail_price_tag[tag].text
-            price = [string_deal_price,string_retail_price]
+        all_price = self.soup.find_all("div",{"class":"a-column a-span7"})
+        for price in all_price:
+            deal_price_tag = price.find("span",{"class":"a-size-base a-color-price s-price a-text-bold"})
+            retail_price_tag = price.find("span",{"class":"a-size-small a-color-secondary a-text-strike"})
+            
+            
+            # if deal price with diff class name
+            if deal_price_tag == None:
+                deal_price_tag = price.find("span",{"class":"a-size-base a-color-price a-text-bold"})
+            
+            # if retail price isn't mentioned
+            if retail_price_tag == None:
+                retail_price_tag = deal_price_tag
+                
+            try:
+                string_deal_price = deal_price_tag.text
+                string_retail_price = retail_price_tag.text
+            except AttributeError:
+                string_deal_price = '0'
+                string_retail_price = '0'
+                print("Warning new class encountered please add in your list!!")
+            price_in_str = [string_deal_price,string_retail_price]
             price_in_int = []
-        
+            
             for i in range(2):
                 # cleaning price
-                string_price = re.sub('\\xa0\\xa0','',price[i])
+                string_price = re.sub('\\xa0\\xa0','',price_in_str[i])
                 string_price = re.sub(',','',string_price)
                 price_in_int.append(int(string_price))
-
-            # storing cleaned price
+            
             deal_price.append(price_in_int[0])
             retail_price.append(price_in_int[1])
-            
+        
         return deal_price,retail_price
+            
+    
+    def extract_rating_from_string(self,string):
+        if string == "":
+            result = "0.0"
+        else:
+            string = list(string)
+            result = "".join(string[0:3])
+        try:
+            result = float(result)
+        except:
+            result = float(string[0] + ".0")
+        return result
+    
+    def extract_rating_n_count_for_blanks(self,tag):
+        # This function goes to link of mobile and scraps the customer count and ratings
+        # else it assigns them zero
+        customer_count = '0'
+        rating = 0
+        link_tag = tag.find("a",{"class":"a-size-small a-link-normal a-text-normal"})
+        link = link_tag["href"]
+        scraper2 = Amazon_all_mobile_scraper()
+        scraper2.setUrl(link)
+        scraper2.load_amazon()
+        # scrap the customer count then the rating
+        var = scraper2.soup.find('h2',{'data-hook':'total-review-count'})
+        try:
+            if var.text != 'No customer reviews':
+                customer_count = re.sub(' customer reviews','',var.text)
+                string_rating = scraper2.soup.find('span',{'data-hook':'rating-out-of-text'})
+                #print(string_rating)
+                rating = float(re.sub(' out of 5 stars','',string_rating.text))
+                
+        except:
+            print("Something went wrong in extract_rating_n_count_for_blanks!")
+            traceback.print_exc()
         
-        
+        scraper2.exitAmazon()
+        return rating,customer_count
+    
+    def extract_rating_n_count(self):
+        var = self.soup.find_all("div",{"class":"a-column a-span5 a-span-last"})
+        rating = []
+        count = []
+        for tag in var:
+            rating_tag = tag.find("span",{"class":"a-icon-alt"})
+            if rating_tag == None:
+                string = ""
+            else:
+                string = rating_tag.text
+            rating_from_string =self.extract_rating_from_string(string)
+            
+            count_tag = tag.find("a",{"class":"a-size-small a-link-normal a-text-normal"})
+            count_from_string = count_tag == None and "" or count_tag.text
+            
+            # If no review or customer count scraped go to the
+            # specific website and scrap it
+            if count_from_string == 'See Details':
+                rating_from_string,count_from_string = self.extract_rating_n_count_for_blanks(tag)
+                
+            rating.append(rating_from_string)
+            count.append(count_from_string)
+        return rating,count 
+
+    
     def exitAmazon(self):
         self.driver.close()
 
+
+data = pd.DataFrame(columns = ['Mobile_names'])
+deal_price = pd.DataFrame(columns = ['Deal_price'])
+retail_price = pd.DataFrame(columns = ['Retail_price'])
+mobile_rating = pd.DataFrame(columns = ['Mobile_rating'])
+customer_count = pd.DataFrame(columns = ['Customers'])
+
 scraper = Amazon_all_mobile_scraper()
 scraper.load_amazon()
-data = scraper.extract_list_of_mobiles()
-data_link = scraper.extract_url_of_mobiles(data)
-data_img = scraper.extract_images_of_mobiles()
-deal_price, retail_price = scraper.extract_price_of_mobile()
+scraper.extract_next_page_url()
+URL_TO_NEXT_PAGE = scraper.get_page_link()
+
+# Crawling happens here.
+for next_page in URL_TO_NEXT_PAGE:
+    # Loading that page
+    scraper.setUrl(next_page)
+    scraper.load_amazon()
+    # Do the scraping for that page
+    data = data.append(pd.DataFrame(data = scraper.extract_list_of_mobiles(),
+                                    columns = ['Mobile_names']),ignore_index = True)
+    #data_link = scraper.extract_url_of_mobiles(data)
+    #data_img = scraper.extract_images_of_mobiles()
+    deal, retail = scraper.extract_price_of_mobile2()
+    rating,count = scraper.extract_rating_n_count()
+    deal_price = deal_price.append(pd.DataFrame(data = deal,
+                                    columns = ['Deal_price']),ignore_index = True)
+    retail_price = retail_price.append(pd.DataFrame(data = retail,
+                                    columns = ['Retail_price']),ignore_index = True)
+    mobile_rating = mobile_rating.append(pd.DataFrame(data = rating,
+                                    columns = ['Mobile_rating']),ignore_index = True)
+    customer_count = customer_count.append(pd.DataFrame(data = count,
+                                    columns = ['Customers']),ignore_index = True)
+    
+    
+    
+    
 scraper.exitAmazon()
 
-# Removing commas in mobile names
+# Removing commas in mobile names and customers
 NUM_OF_MOBILES = len(data)
 mobile_names = []
 for i in range(NUM_OF_MOBILES):
-    mobile_names.append(re.sub(",","",data[i]))
+    data.iloc[i,0] = re.sub(",","",data.iloc[i,0])
+    customer_count.iloc[i,0] = re.sub(",","",customer_count.iloc[i,0])
+            
+        
+# combining the scraped data
+mobilephone_dataset = pd.DataFrame()
+mobilephone_dataset = mobilephone_dataset.assign(
+        Mobile_name = data,
+        Deal_price = deal_price,
+        Retail_price = retail_price,
+        Mobile_rating = mobile_rating,
+        Customers = customer_count
+        )
 
-# Converting to csv file    
-with open('mobilename_link.csv','a') as f:
-    f.write('Mobile Name,Link,Image_location,Deal_price,Retail_price\n')
-    for i in range(NUM_OF_MOBILES):
-        f.write(mobile_names[i] + "," + data_link[i] + "," + data_img[i][0] + "," +  str(deal_price[i]) + "," + str(retail_price[i]) +"\n")
+# converting price columns to int 
+try:
+    mobilephone_dataset['Deal_price'] = pd.to_numeric(mobilephone_dataset['Deal_price'])
+    mobilephone_dataset['Retail_price'] = pd.to_numeric(mobilephone_dataset['Retail_price'])
+    mobilephone_dataset['Customers'] = pd.to_numeric(mobilephone_dataset['Customers'])
+except:
+    print('Data set not proper.\nSome data in price columns are not numeric!')      
         
-        
-        
-        
-        
+
+# saving the dataset
+mobilephone_dataset.to_csv('mobilename_link.csv')
 
 
 
